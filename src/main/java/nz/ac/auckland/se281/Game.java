a7ac084d7d867eac7cfe9b76c4e557149de5231b
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-15 00:46:14 +1200

    added new fields and wrote the code for printing out message when number of fingers is correct

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -7,17 +7,35 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   private int roundNumber = 0;
+  private String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    playerName = options[0];
   }
 
   public void play() {
+
+    boolean correctFingers = false;
+
     roundNumber++;
+
     MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
     MessageCli.ASK_INPUT.printMessage();
-    MessageCli.INVALID_INPUT.printMessage();
+
+    String input = Utils.scanner.nextLine();
+    for (int i = 0; i <= 5; i++) {
+      if (input.equals(Integer.toString(i))) {
+        correctFingers = true;
+      }
+    }
+
+    if (correctFingers) {
+      MessageCli.PRINT_INFO_HAND.printMessage(playerName, input);
+    } else {
+      MessageCli.INVALID_INPUT.printMessage();
+    }
   }
 
   public void endGame() {}
