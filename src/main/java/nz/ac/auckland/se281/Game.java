b9e4a2468942eb39183e5cf49f692ee5fa823d61
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-14 23:44:14 +1200

    wrote code for print the round number

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int roundNumber = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    roundNumber++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
+
+  }
 
   public void endGame() {}
 
