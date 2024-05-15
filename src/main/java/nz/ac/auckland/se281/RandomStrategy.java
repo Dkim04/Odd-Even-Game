4f01031a95c388b03f35acb68b503a9712a4b7d0
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-16 00:43:23 +1200

    created the top strategy and also made it print random or top when method used to check if my code was working properly

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -4,6 +4,7 @@ public class RandomStrategy implements Strategy {
 
   @Override
   public int getFingers() {
+    System.out.println("random");
     return Utils.getRandomNumberRange(0, 5);
   }
 }
--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -1,13 +1,48 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
+import nz.ac.auckland.se281.Main.Choice;
 
 public class TopStrategy implements Strategy {
 
-  public TopStrategy(ArrayList<Integer> playerHistory) {}
+  private int amountofEvens = 0;
+  private int amountofOdds = 0;
+  private int fingers;
+
+  public TopStrategy(ArrayList<Integer> playerHistory, Choice choice) {
+    for (int i : playerHistory) {
+      if (Utils.isEven(i)) {
+        amountofEvens++;
+      } else if (Utils.isOdd(i)) {
+        amountofOdds++;
+      }
+    }
+
+    if (choice == Choice.EVEN) {
+
+      if (amountofEvens > amountofOdds) {
+        fingers = Utils.getRandomOddNumber();
+      } else if (amountofEvens < amountofOdds) {
+        fingers = Utils.getRandomEvenNumber();
+      } else if (amountofEvens == amountofOdds) {
+        fingers = Utils.getRandomNumberRange(0, 5);
+      }
+
+    } else if (choice == Choice.ODD) {
+
+      if (amountofEvens > amountofOdds) {
+        fingers = Utils.getRandomEvenNumber();
+      } else if (amountofEvens < amountofOdds) {
+        fingers = Utils.getRandomOddNumber();
+      } else if (amountofEvens == amountofOdds) {
+        fingers = Utils.getRandomNumberRange(0, 5);
+      }
+    }
+  }
 
   @Override
   public int getFingers() {
-    return 0;
+    System.out.println("top");
+    return fingers;
   }
 }
