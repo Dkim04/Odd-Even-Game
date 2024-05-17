8cd769686505ace12ef3d3d20b5067305283aca8
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 18:14:05 +1200

    added javadoc comments to EasyDifficulty class

--- src/main/java/nz/ac/auckland/se281/EasyDifficulty.java
+++ src/main/java/nz/ac/auckland/se281/EasyDifficulty.java
@@ -4,15 +4,21 @@ public class EasyDifficulty implements RobotDifficulty {
 
   private Strategy strategy;
 
+  /**
+   * This constructor creates a new EasyDifficulty object setting the strategy to a new
+   * RandomStrategy
+   */
   public EasyDifficulty() {
     this.strategy = new RandomStrategy();
   }
 
+  /** This method returns the strategy that the robot will use. */
   @Override
   public Strategy getStrategy() {
     return strategy;
   }
 
+  /** This method returns the number of fingers that the robot will put out. */
   @Override
   public int play() {
     return Utils.getRandomNumberRange(0, 5);
