176793e44db121d465c5b286b0811c4b71cb2b09
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 18:19:20 +1200

    added javadoc comments for HardDifficulty class

--- src/main/java/nz/ac/auckland/se281/HardDifficulty.java
+++ src/main/java/nz/ac/auckland/se281/HardDifficulty.java
@@ -7,6 +7,16 @@ public class HardDifficulty implements RobotDifficulty {
 
   private Strategy strategy;
 
+  /**
+   * This constructor creates a new HardDifficulty object setting the strategy to a new
+   * RandomStrategy if the round number is less than or equal to 3, otherwise it sets the strategy
+   * to a new TopStrategy if the robot won the previous round and the last strategy was a
+   * TopStrategy, or it sets the strategy to a new RandomStrategy if the robot won the previous
+   * round and the last strategy was a RandomStrategy, or it sets the strategy to a new TopStrategy
+   * if the robot lost the previous round and the last strategy was a RandomStrategy, or it sets the
+   * strategy to a new RandomStrategy if the robot lost the previous round and the last strategy was
+   * a TopStrategy.
+   */
   public HardDifficulty(
       Choice choice,
       int roundNumber,
@@ -33,14 +43,17 @@ public class HardDifficulty implements RobotDifficulty {
     }
   }
 
+  /** This method sets the strategy that the robot will use. */
   public void setStrategy(Strategy strategy) {
     this.strategy = strategy;
   }
 
+  /** This method returns the strategy that the robot will use. */
   public Strategy getStrategy() {
     return strategy;
   }
 
+  /** This method returns the number of fingers that the robot will put out. */
   public int play() {
     return strategy.getFingers();
   }
