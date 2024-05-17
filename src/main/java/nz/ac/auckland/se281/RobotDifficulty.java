43d741a1f9ce5800777ea13b40eb666f178c87ba
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 18:04:22 +1200

    added javadoc comments to RobotDifficulty interface

--- src/main/java/nz/ac/auckland/se281/RobotDifficulty.java
+++ src/main/java/nz/ac/auckland/se281/RobotDifficulty.java
@@ -1,8 +1,11 @@
 package nz.ac.auckland.se281;
 
+/** This interface represents the different difficulties that the robot can have. */
 public interface RobotDifficulty {
 
+  /** This method returns the strategy that the robot will use. */
   public Strategy getStrategy();
 
+  /** This method returns the number of fingers that the robot will put out. */
   public int play();
 }
