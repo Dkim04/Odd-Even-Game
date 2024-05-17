f3c0e7e92b893dfd64bb95a8c006e41f6f9bf254
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 17:47:12 +1200

    wrote javadoc for RandomStrategy class

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se281;
 
 public class RandomStrategy implements Strategy {
 
+  /** This method returns a random number of fingers between 0 and 5 using the random method. */
   @Override
   public int getFingers() {
     System.out.println("random");
