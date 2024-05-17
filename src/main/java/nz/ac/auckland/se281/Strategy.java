59fe8ee9e1a3f225c95f3d4edd3ba430fa0eb3dd
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 17:43:05 +1200

    wrote the java doc comment for strategy interface

--- src/main/java/nz/ac/auckland/se281/Strategy.java
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -1,5 +1,11 @@
 package nz.ac.auckland.se281;
 
+/**
+ * This interface represents the different strategies that the robot can use depending on the
+ * difficulty level.
+ *
+ * <p>getFingers() returns the number of fingers that the robot will put out.
+ */
 public interface Strategy {
   public int getFingers();
 }
