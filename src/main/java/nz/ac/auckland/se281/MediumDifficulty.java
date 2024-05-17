1a8e46d559fa64117ec95b38a8a6a748b8c6dcf7
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-17 18:15:40 +1200

    added javadoc comments to MediumDifficulty class

--- src/main/java/nz/ac/auckland/se281/MediumDifficulty.java
+++ src/main/java/nz/ac/auckland/se281/MediumDifficulty.java
@@ -7,6 +7,11 @@ public class MediumDifficulty implements RobotDifficulty {
 
   private Strategy strategy;
 
+  /**
+   * This constructor creates a new MediumDifficulty object setting the strategy to a new
+   * RandomStrategy if the round number is less than or equal to 3, otherwise it sets the strategy
+   * to a new TopStrategy.
+   */
   public MediumDifficulty(Choice choice, int roundNumber, ArrayList<Integer> playerHistory) {
     if (roundNumber <= 3) {
       setStrategy(new RandomStrategy());
@@ -15,15 +20,18 @@ public class MediumDifficulty implements RobotDifficulty {
     }
   }
 
+  /** This method sets the strategy that the robot will use. */
   public void setStrategy(Strategy strategy) {
     this.strategy = strategy;
   }
 
+  /** This method returns the strategy that the robot will use. */
   @Override
   public Strategy getStrategy() {
     return strategy;
   }
 
+  /** This method returns the number of fingers that the robot will put out. */
   @Override
   public int play() {
     return strategy.getFingers();
